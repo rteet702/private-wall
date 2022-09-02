d2ff80fdb42de817e2b4ea04c6a766f5c195788c
Robert Teets <teetsrobert@gmail.com>
2022-09-02 09:39:22 -0600

    3: registration validations complete

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,7 +1,16 @@
 from flask_app import app
-from flask import render_template
+from flask_app.models.user import User
+from flask import render_template, request, redirect
 
 
 @app.route('/')
-def landing():
+def r_landing():
     return render_template('landing.html')
+
+@app.route('/register', methods=['POST'])
+def f_register():
+    if User.validate_registration(request.form):
+        print('Valid registration')
+    else:
+        print('Invalid registration')
+    return redirect('/')
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,3 +1,12 @@
+import re
+from flask import flash
+
+
+NAME_REGEX = re.compile(r'^[a-zA-Z]{2,}$')
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9+_-]+@[a-zA-Z._-]+\.[a-zA-Z]+$')
+PASSWORD_REGEX = re.compile(r'^[a-zA-Z0-9!@#$%^&*()+_-].{8,}$')
+
+
 class User:
     def __init__(self, data:dict) -> None:
         self.id = data.get('id')
@@ -7,3 +16,25 @@ class User:
         self.password = data.get('password')
         self.created_at = data.get('created_at')
         self.updated_at = data.get('updated_at')
+
+    @staticmethod
+    def validate_registration(form:dict) -> bool:
+        is_valid = True
+
+        if not NAME_REGEX.match(form.get('first_name')):
+            flash('* First name must be at least 2 characters long.')
+            is_valid = False
+        if not NAME_REGEX.match(form.get('last_name')):
+            flash('* Last name must be at least 2 characters long.')
+            is_valid = False
+        if not EMAIL_REGEX.match(form.get('email')):
+            flash('* Email must be valid.')
+            is_valid = False
+        if not PASSWORD_REGEX.match(form.get('password')):
+            flash('* Password must be at least 8 characters!')
+            is_valid = False
+        if form.get('confirm_password') != form.get('password'):
+            flash('* Passwords do not match.')
+            is_valid = False
+
+        return is_valid
